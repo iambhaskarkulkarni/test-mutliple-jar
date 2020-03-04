09294e65b4c11aab16ac1c9a0610db477176cf07
bhaskar.kulkarni <bhaskar.kulkarni@STS-1371.sts.co.in>
2020-03-04 18:22:14 +0530

    Second Commit

--- email-sender/src/test/java/com/sixdee/emailsender/EmailSenderApplicationTests.java
+++ email-sender/src/test/java/com/sixdee/emailsender/EmailSenderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sixdee.emailsender;
+/*package com.sixdee.emailsender;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class EmailSenderApplicationTests {
 	}
 
 }
+*/
