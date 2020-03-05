19d0fb0f240f956bf245344ab6672e19bcc9ba4c
bhaskar.kulkarni <bhaskar.kulkarni@STS-1371.sts.co.in>
2020-03-05 12:39:31 +0530

    test

--- notification-details/src/main/java/com/sixdee/notificationdetails/controller/NotificationDetailsController.java
+++ notification-details/src/main/java/com/sixdee/notificationdetails/controller/NotificationDetailsController.java
@@ -2,11 +2,13 @@ package com.sixdee.notificationdetails.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.sixdee.notificationdetails.dto.EmailDTO;
+import com.sixdee.emailsender.dto.EmailDTO;
+
+
+
 
 @RestController
 public class NotificationDetailsController {
