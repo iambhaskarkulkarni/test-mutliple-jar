8bfaa9a8c643f3a2627a28ed1d5f9061bdc8ac92
Bhaskar Kulkarni <23276373+iambhaskarkulkarni@users.noreply.github.com>
2020-03-05 15:38:27 +0530

    Update NotificationDetailsController.java
    tt

--- notification-details/src/main/java/com/sixdee/notificationdetails/controller/NotificationDetailsController.java
+++ notification-details/src/main/java/com/sixdee/notificationdetails/controller/NotificationDetailsController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-//import com.sixdee.emailsender.dto;
+import com.sixdee.emailsender.dto.EmailDTO;
 
 
 @RestController
@@ -14,13 +14,13 @@ public class NotificationDetailsController {
 	private RestTemplate restTemplate;
 	
 	@GetMapping("/send-email-notificationssss")
-	/*public EmailDTO sendEmailNotification() {
+	public EmailDTO sendEmailNotification() {
 		return restTemplate.getForObject("http://email-sender-service/send-email-notification", EmailDTO.class);
-	}*/
-	
-	
-	public void sendEmailNotification1() {
-		//return restTemplate.getForObject("http://email-sender-service/send-email-notification", EmailDTO.class);
 	}
+	
+	
+	/* public void sendEmailNotification1() {
+		//return restTemplate.getForObject("http://email-sender-service/send-email-notification", EmailDTO.class);
+	} */
 
 }
